9b848d065db7b3def19312bf18d57aaa2a4b0bb6
Andrey Bacherov <bandvig@mail.ru>
2019-01-26 20:03:09 +0300

    Add monitor for MAROCCHINO (port Stafford Horne's modifications from upstream).

--- bench/verilog/mor1kx_monitor.v
+++ bench/verilog/mor1kx_monitor.v
@@ -25,15 +25,14 @@
 `ifndef CPU_WRAPPER
  `define CPU_WRAPPER `MOR1KX_INST.mor1kx_cpu
 `endif
-`define CPU_INST `CPU_WRAPPER.`MOR1KX_CPU_PIPELINE.mor1kx_cpu
 `define EXECUTE_STAGE_INSN `CPU_WRAPPER.monitor_execute_insn
 `define EXECUTE_STAGE_ADV `CPU_WRAPPER.monitor_execute_advance
 `define CPU_clk `CPU_WRAPPER.monitor_clk
 `define CPU_FLAG `CPU_WRAPPER.monitor_flag
 `define CPU_SR `CPU_WRAPPER.monitor_spr_sr
 `define EXECUTE_PC `CPU_WRAPPER.monitor_execute_pc
-`define GPR_GET(x) `CPU_INST.get_gpr(x)
-`define GPR_SET(x, y) `CPU_INST.set_gpr(x, y)
+`define GPR_GET(x) `CPU_WRAPPER.get_gpr(x)
+`define GPR_SET(x, y) `CPU_WRAPPER.set_gpr(x, y)
 
 `include "mor1kx-defines.v"
 
@@ -909,39 +908,6 @@ module mor1kx_monitor #(parameter LOG_DIR= "../out") ();
 		    $sformat(insnstring, "l.csync");
 		endcase // case (insn[`OR1K_SYSTRAPSYNC_OPC_SELECT])
 	     end
-       `OR1K_OPCODE_FPU:
-         begin
-             case (insn[`OR1K_FPUOP_SELECT])
-                 `OR1K_FPUOP_ADD:
-                  $sformat(insnstring, "lf.add.s   r%0d,r%0d,r%0d",rD_num,rA_num,rB_num);
-                 `OR1K_FPUOP_SUB:
-                  $sformat(insnstring, "lf.sub.s   r%0d,r%0d,r%0d",rD_num,rA_num,rB_num);
-                 `OR1K_FPUOP_MUL:
-                  $sformat(insnstring, "lf.mul.s   r%0d,r%0d,r%0d",rD_num,rA_num,rB_num);
-                 `OR1K_FPUOP_DIV:
-                  $sformat(insnstring, "lf.div.s   r%0d,r%0d,r%0d",rD_num,rA_num,rB_num);
-                 `OR1K_FPUOP_ITOF:
-                  $sformat(insnstring, "lf.itof.s  r%0d,r%0d",rD_num,rA_num);
-                 `OR1K_FPUOP_FTOI:
-                  $sformat(insnstring, "lf.ftoi.s  r%0d,r%0d",rD_num,rA_num);
-                 `OR1K_FPUOP_REM:
-                  $sformat(insnstring, "lf.rem.s   r%0d,r%0d,r%0d",rD_num,rA_num,rB_num);
-                 `OR1K_FPCOP_SFEQ:
-                  $sformat(insnstring, "lf.sfeq.s   r%0d,r%0d",rA_num,rB_num);
-                 `OR1K_FPCOP_SFNE:
-                  $sformat(insnstring, "lf.sfne.s   r%0d,r%0d",rA_num,rB_num);
-                 `OR1K_FPCOP_SFGT:
-                  $sformat(insnstring, "lf.sfgt.s   r%0d,r%0d",rA_num,rB_num);
-                 `OR1K_FPCOP_SFGE:
-                  $sformat(insnstring, "lf.sfge.s   r%0d,r%0d",rA_num,rB_num);
-                 `OR1K_FPCOP_SFLT:
-                  $sformat(insnstring, "lf.sflt.s   r%0d,r%0d",rA_num,rB_num);
-                 `OR1K_FPCOP_SFLE:
-                  $sformat(insnstring, "lf.sfle.s   r%0d,r%0d",rA_num,rB_num);
-                 default:
-                  $sformat(insnstring, "%t: FPU opcode 0x%0h, r%0d,r%0d,r%0d", $time, opcode, rD_num,rA_num,rB_num);
-             endcase // case(insn[`OR1K_FPUOP_SELECT])
-         end
 	   default:
 	     begin
 		$sformat(insnstring, "%t: Unknown opcode 0x%0h",$time,opcode);
